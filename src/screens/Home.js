fc02267f0ef7aff325f6abbc1928c675274812fa
ssz\saiprakash <sai-prakash-reddy.pulakunta@dxc.com>
2019-09-25 18:16:39 +0530

    added app logo

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,15 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {PureComponent} from 'react';
-import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+  Image,
+} from 'react-native';
 import Label from '../components/Label';
+import Logo from '../assets/Logo.png';
 
 export default class Home extends PureComponent {
   state = {
@@ -8,11 +17,6 @@ export default class Home extends PureComponent {
     casing: 0,
     transport: 500,
   };
-
-  componentDidMount() {
-    // this.calulate(900);
-    // console.warn('hello');
-  }
 
   handleRods = value => {
     this.setState({rods: value});
@@ -27,26 +31,19 @@ export default class Home extends PureComponent {
   };
 
   handlesubmit = () => {
-    console.warn('submit');
     let length = this.state.rods * 20;
     let finalprice = this.calulate(length);
     alert(finalprice);
   };
 
   calulate = n => {
-    console.warn('total length', n);
     let sum = 300 * 80;
     let minlength = 400;
     let basicincrement = 6;
     let count = 0;
     let basicprice = 80;
     let lastlength = 300;
-
     if (n <= 300) {
-      console.warn(
-        'lessthan 300 feet',
-        n * 80 + this.state.casing * 30 + this.state.transport,
-      );
       return n * 80 + this.state.casing * 30 + this.state.transport;
     } else {
       while (minlength <= n) {
@@ -76,64 +73,59 @@ export default class Home extends PureComponent {
       basicprice = basicprice + basicincrement;
       sum = sum + (n - lastlength) * basicprice;
     }
-    console.warn(
-      'finalprice------',
-      sum + this.state.casing * 30 + this.state.transport,
-    );
+    // console.warn(
+    //   'finalprice------',
+    //   sum + this.state.casing * 30 + this.state.transport,
+    // );
     return sum + this.state.casing * 30 + this.state.transport;
   };
 
   render() {
-    console.warn('rods', this.state.casing);
     return (
       <View style={styles.container}>
-        <Label
-          label="No. of Rods"
-          keyboardType="numeric"
-          value={this.state.rods}
-          onChangeText={this.handleRods}
-        />
-
-        <Label
-          style={styles.input}
-          label="Casing"
-          underlineColorAndroid="transparent"
-          keyboardType="numeric"
-          onChangeText={this.handleCasing}
-        />
-        <Label
-          style={styles.input}
-          underlineColorAndroid="transparent"
-          keyboardType="numeric"
-          label="Transport"
-          initial={this.state.transport}
-          onChangeText={this.handleTransport}
-        />
-
-        <TouchableOpacity
-          style={styles.submitButton}
-          onPress={() => this.handlesubmit()}>
-          <Text style={styles.submitButtonText}> Submit </Text>
-        </TouchableOpacity>
-
-        {/* <Text> Ajay Drillers </Text>
-        <View>
-          <TextInput value={rods} />
-          <Text>Rods :</Text>
+        <View style={styles.topstyle}>
+          <View
+            style={{
+              width: 100,
+              height: 100,
+              backgroundColor: '#fff',
+              borderRadius: 100,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Image source={Logo} style={{width: 70, height: 70}} />
+          </View>
         </View>
-        <View>
-          <TextInput onChangeText={this.handleCasing()} />
-          <Text>Casing :</Text>
+        <View style={{padding: 20}}>
+          <Text>No.of Rods</Text>
+          <TextInput
+            style={styles.input}
+            label="No. of Rods"
+            keyboardType="numeric"
+            value={this.state.rods}
+            onChangeText={this.handleRods}
+          />
+          <Label
+            style={styles.input}
+            label="Casing"
+            underlineColorAndroid="transparent"
+            keyboardType="numeric"
+            onChangeText={this.handleCasing}
+          />
+          <Label
+            style={styles.input}
+            underlineColorAndroid="transparent"
+            keyboardType="numeric"
+            label="Transport"
+            initial={this.state.transport}
+            onChangeText={this.handleTransport}
+          />
+          <TouchableOpacity
+            style={styles.submitButton}
+            onPress={() => this.handlesubmit()}>
+            <Text style={styles.submitButtonText}> Submit </Text>
+          </TouchableOpacity>
         </View>
-        <View>
-          <TextInput onChangeText={this.handleTransport()} />
-          <Text>Transport :</Text>
-        </View>
-
-        <TouchableOpacity onPress={() => this.handlesubmit()}>
-          Submit
-        </TouchableOpacity>
-        <Text>{this.state.grandTotal}</Text> */}
       </View>
     );
   }
@@ -141,7 +133,7 @@ export default class Home extends PureComponent {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 23,
+    paddingTop: 0,
   },
   input: {
     margin: 15,
@@ -157,5 +149,11 @@ const styles = StyleSheet.create({
   },
   submitButtonText: {
     color: 'white',
+  },
+  topstyle: {
+    height: 175,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
